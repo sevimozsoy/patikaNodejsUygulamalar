b2416719d500c820dcf4bc47d6999bfd20e51d47
sevimozsoy <sevimozsoy30@gmail.com>
2022-05-23 17:03:27 +0300

    Configuring ejs and express's midleware functions.

--- CleanBlog/app.js
+++ CleanBlog/app.js
@@ -2,13 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.static('public'));
+
+
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog Title',
-    description: 'Blog Description',
-  };
-  res.send(blog);
+  res.sendFile('./temp/index.html')
 });
 
 const port = 3000;
--- PCat/app.js
+++ PCat/app.js
@@ -1,15 +1,27 @@
 import express from 'express';
+import path from 'path';
+import ejs from 'ejs';
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARE
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  const photo = {
-      id: 1,
-      name: "Photo Name",
-      description:"Photo Description"
-  }
-    res.send(photo);
+  //res.sendFile(path.resolve('temp/index.html'));
+  res.render('index');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact',(req,res) => {
+  res.render('contact')
+})
 
 const port = 3000;
 app.listen(port, () => {
