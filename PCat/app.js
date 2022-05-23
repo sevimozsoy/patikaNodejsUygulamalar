3dc36813d6f09592e52f52f4fe737b44b5022023
sevimozsoy <sevimozsoy30@gmail.com>
2022-05-23 11:03:13 +0300

    Created two separate project. PCat is a photo app followed frompatika's Nodejs course. CleanBlog is PCat's cloneish.

---
+++ PCat/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const photo = {
+      id: 1,
+      name: "Photo Name",
+      description:"Photo Description"
+  }
+    res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
